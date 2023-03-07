295ca22d48780ee92c91cb6de2d4c74f5eaa7648
Thiago Levi <thiago.levi@outlook.com>
2023-03-07 17:33:27 -0300

    add exercicio 14

--- 13 - repeticao-espacada/exercicios/exercicio-14/app.js
+++ 13 - repeticao-espacada/exercicios/exercicio-14/app.js
@@ -5,7 +5,10 @@
     maiúsculas.
 */
 
-
+const mainTitle = document.querySelector(`[data-js="main-title"]`);
+mainTitle.innerHTML = mainTitle.textContent.toLocaleUpperCase();
+//ou
+mainTitle.classList.add("upper-case");
 
 /*
   02
@@ -14,7 +17,20 @@
   - Cada número deve estar dentro de uma <li> com a classe "number".
 */
 
-const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
+const numbers = [53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55];
+const ulOfNumbers = document.querySelector(`[data-js="ul-of-numbers"]`);
+numbers.forEach((number) => {
+  ulOfNumbers.innerHTML += `<li>${number}</li>`;
+});
+
+// ou
+
+// const renderNumber = numbers.reduce(
+//   (templateHtml, number) => (templateHtml += `<li>${number}</li>`),
+//   ``
+// );
+
+//ulOfNumbers.innerHTML = renderNumber;
 
 /*
   03
@@ -24,7 +40,17 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     - Se o número é ímpar, exiba-o na cor "pink".
 */
 
+//const listOfNumbers = Array.from(ulOfNumbers);
+const liWithANumber = document.querySelectorAll("li");
 
+function isEven(number) {
+  return number % 2 === 0;
+}
+liWithANumber.forEach((li) => {
+  isEven(li.textContent)
+    ? li.classList.add("color-of-even")
+    : li.classList.add("color-of-odd");
+});
 
 /*
   04
@@ -34,8 +60,8 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   P.s: a classe "body-background" já está declarada no style.css.
 */
 
-
-
+const body = document.querySelector(`[data-js="document-body"]`);
+body.classList.add("body-background");
 /*
   05
 
@@ -44,7 +70,9 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     do link do index.html.
 */
 
+const link = document.querySelector('[data-js="link"]');
 
+link.href = "https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo";
 
 /*
   06
@@ -52,7 +80,7 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Exiba o novo valor do atributo href do link no console.
 */
 
-
+console.log(link.href);
 
 /*
   07
@@ -61,7 +89,7 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     manipuladas via JS no h1.
 */
 
-
+console.log(mainTitle.style);
 
 /*
   08
@@ -69,7 +97,7 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Remova a classe "body-background", do elemento body.
 */
 
-
+body.classList.remove("body-background");
 
 /*
   09
@@ -77,3 +105,13 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Se o link da página possuir uma classe "link", remova-a;
   - Não utilize o método remove() para fazer isso.
 */
+
+console.log(link.classList);
+
+link.classList.toggle("link");
+
+console.log(link.classList);
+
+link.classList.toggle("link");
+
+console.log(link);
